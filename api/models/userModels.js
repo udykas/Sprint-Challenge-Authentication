8ddc3fecd906ccffe41efc669d0103a793e3998e
udykas <udykas@gmail.com>
2018-06-15 12:03:45 -0400

    User Model set up, including the pre hook and chackPassword

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -5,16 +5,28 @@ const Schema = mongoose.Schema;
 const SALT_ROUNDS = 11;
 
 const UserSchema = Schema({
-  // create your user schema here.
-  // username: required, unique and lowercase
-  // password: required
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 UserSchema.pre('save', function(next) {
-  // https://github.com/kelektiv/node.bcrypt.js#usage
-  // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
-  // if there is an error here you'll need to handle it by calling next(err);
-  // Once the password is encrypted, call next() so that your userController and create a user
+  return bcrypt
+    .hash(this.password, 12)
+    .then(hash => {
+      this.password = hash;
+      return next();
+    })
+    .catch(err => {
+      return next(err);
+    })
 });
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
@@ -22,6 +34,7 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  return bcrypt.compare(plainTextPW, this.password);
 };
 
 module.exports = mongoose.model('User', UserSchema);
