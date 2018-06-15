94557650db09426a2f07827997265d504fbf9002
udykas <udykas@gmail.com>
2018-06-15 13:22:00 -0400

    Added cors to server

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const routes = require('./api/routes/routes');
 
 const server = express();
 const corsOptions = {
-  // If you're moving onto the stretch problem you'll need to set this obj with the appropriate fields
-  // ensure that your client's URL/Port can achieve a Handshake
-  // then pass this object to the cors() function
+  origin: 'http://localhost:3000',
+  credentials: true,
 };
 
 server.use(bodyParser.json());
-server.use(cors());
+server.use(cors(corsOptions));
 
 routes(server);
 
