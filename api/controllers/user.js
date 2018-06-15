7a49850fded2d1ddf582f89f22140501402fa17c
udykas <udykas@gmail.com>
2018-06-15 12:18:01 -0400

    Step 2 is complete, can create user with hashed password

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,6 +3,14 @@ const bcrypt = require('bcrypt');
 
 const createUser = (req, res) => {
   const { username, password } = req.body;
+  User
+    .create(req.body)
+    .then(user => {
+      res.status(200).json({ username: user.username });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
 };
